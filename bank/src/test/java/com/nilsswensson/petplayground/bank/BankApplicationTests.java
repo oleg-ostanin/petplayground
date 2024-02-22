383191b0d270cdd12a46daa007cbc46e8c2a6968
oleg-ostanin <oleg.alex.ostanin@gmail.com>
2024-02-22 15:54:47 +0300

    add modules

---
+++ bank/src/test/java/com/nilsswensson/petplayground/bank/BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilsswensson.petplayground.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
